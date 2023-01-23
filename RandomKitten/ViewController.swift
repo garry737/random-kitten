2b6f87f7da0d941b99343ba177a8b53795068316
garry737 <garryfanata@gmail.com>
2023-01-24 03:32:54 +0700

    Initial Commit

---
+++ RandomKitten/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomKitten
+//
+//  Created by Garry Fanata on 1/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
